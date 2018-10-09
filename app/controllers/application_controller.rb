084a9e60b9179b4932d08713e7db5c66173dc5b3
sweinstein27 <sweinstein27@gmail.com>
2018-10-09 07:21:06 -0700

    shopping cart complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # before_action :current_user
   # before_action :require_logged_in, except: [:new, :create, :home]
-  helper_method :current_user
+  # helper_method :current_user
   helper_method :current_order
 
 
@@ -24,8 +24,8 @@ class ApplicationController < ActionController::Base
       redirect_to root_path unless logged_in?
     end
 
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
+    # def current_user
+    #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    # end
     
 end
